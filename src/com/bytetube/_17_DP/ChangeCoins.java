6a26f6fb12b5f6a8c83d3bed2ac8191cf1f9f018
isisisisisisithch <dallucus@gmail.com>
2020-06-15 22:06:21 -0400

    add dp

--- src/com/bytetube/_17_DP/ChangeCoins.java
+++ src/com/bytetube/_17_DP/ChangeCoins.java
@@ -9,8 +9,52 @@ package com.bytetube._17_DP;
 public class ChangeCoins {
 
     public static void main(String[] args) {
-        System.out.println(coins2(19));//25,20 ,5,1
+        System.out.println(coins4(19, new int[]{25, 20, 5, 1}));//25,20 ,5,1
+
     }
+
+
+    public static int coins4(int n,int[] faces){
+        if (n < 1|| faces == null || faces.length == 0)  return -1;
+        int[] dp = new int[n+1];//凑够n分需要的硬币数
+
+        for (int i = 1; i <= n; i++) {
+            //dp[i]=min { dp(i – 25), dp(i – 20), dp(i – 5), dp(i – 1) } + 1
+            int min = Integer.MAX_VALUE;
+            for (int face : faces) {
+                if (i < face) continue;
+                min = Math.min(dp[i-face],min);
+            }
+            dp[i] = min + 1;
+        }
+        return dp[n];
+    }
+
+    /**
+     * 递推（自底向上）
+     * 先计算出较小的值，再依次算出较大值
+     * @param n
+     * @return
+     */
+    public static int coins3(int n){
+        if (n < 1)  return -1;
+        int[] dp = new int[n+1];//凑够n分需要的硬币数
+
+        for (int i = 1; i <= n; i++) {
+            //dp[i]=min { dp(i – 25), dp(i – 20), dp(i – 5), dp(i – 1) } + 1
+            int min = dp[i-1];
+
+           // if (i >= 1)  min = Math.min(dp[i-1],min);
+            if (i >= 5)  min = Math.min(dp[i-5],min);
+            if (i >= 20)  min = Math.min(dp[i-20],min);
+            if (i >= 25)  min = Math.min(dp[i-25],min);
+            dp[i] = min + 1;
+        }
+
+        return dp[n];
+    }
+
+
 
     //② 记忆化搜索（自顶向下）
     public static int coins2(int n){
@@ -33,8 +77,8 @@ public class ChangeCoins {
         if (n < 1)  return Integer.MAX_VALUE;
 
         if (dp[n] == 0) {
-            int min1 = Math.min( coins1(n-25),coins1(n-20));
-            int min2 = Math.min( coins1(n-5),coins1(n-1));
+            int min1 = Math.min( coin2(n-25,dp),coin2(n-20,dp));
+            int min2 = Math.min( coin2(n-5,dp),coin2(n-1,dp));
             dp[n] = Math.min(min1,min2)+1;
         }
 
---
+++ src/com/bytetube/_17_DP/MaxSubArray.java
@@ -0,0 +1,50 @@
+package com.bytetube._17_DP;
+
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray2(arr));
+    }
+    public static int maxSubArray2(int[] nums){
+        if (nums == null || nums.length == 0) return 0;
+        //① 定义状态（状态是原问题、子问题的解）//② 设置初始状态（边界）
+        int dp = nums[0];
+        int max = dp;
+        System.out.println("dp[0] = "+dp);
+        //③ 确定状态转移方程
+        for (int i = 1; i < nums.length; i++) {
+            if (dp <= 0) {
+                dp = nums[i];
+            }else {
+                dp = dp + nums[i];
+            }
+
+            max = Math.max(dp,max);
+            System.out.println("dp["+i+"] = "+dp);
+        }
+        return max;
+    }
+
+    public static int maxSubArray1(int[] nums){
+        if (nums == null || nums.length == 0) return 0;
+    //① 定义状态（状态是原问题、子问题的解）
+        int[] dp = new int[nums.length];
+        //② 设置初始状态（边界）
+        dp[0] = nums[0];
+        int max = dp[0];
+        System.out.println("dp[0] = "+dp[0]);
+        //③ 确定状态转移方程
+        for (int i = 1; i < dp.length; i++) {
+            if (dp[i-1] <= 0) {
+                dp[i] = nums[i];
+            }else {
+                dp[i] = dp[i-1] + nums[i];
+            }
+
+            max = Math.max(dp[i],max);
+            System.out.println("dp["+i+"] = "+dp[i]);
+        }
+        return max;
+
+    }
+}
